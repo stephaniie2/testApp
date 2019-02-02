c218921b5c7014033b8037a81bb6320673c6aac2
stephyswe <stephaniiehallberg@gmail.com>
2019-02-02 13:54:04 +0100

    accessing state, getters for derived state, mapping to computed

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>There are {{ catLength }} categories by {{ user.id }}</p>
+    <p>{{ getEventById(2) }}</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters(['getEventById', 'catLength']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style scoped></style>
